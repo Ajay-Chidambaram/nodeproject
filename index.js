49968658140037107748a3142108584b5c6ea8db
ajay <silambu.ajayk@gmail.com>
2021-02-21 10:29:06 +0530

    index page changed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-      res.send('Hello World!');
-      res.send('This is Chidambaram');
+      res.send('Hello World!, This is Chidambaram');
 });
 app.listen(3001, function () {
       console.log('Example app listening on port 3000!');
